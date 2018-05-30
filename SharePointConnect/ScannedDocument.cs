22db80dc24f75289b43e31b246ebc54907bc21fb
Timo Tuchczynski <timo.tuchczynski@nasconception.de>
2018-05-30 13:08:31 +0200

    Dateierweiterung wurde als Feld der Klasse ScannedDocument hinzugefügt.

--- SharePointConnect/ScannedDocument.cs
+++ SharePointConnect/ScannedDocument.cs
@@ -16,7 +16,7 @@ namespace SharePointConnect
         readonly string contactNo;
         readonly string keyword;
         readonly string eventNo;
-
+        readonly string fileExtension;
 
         public ScannedDocument(Dictionary<string,object> scannedDocument, string filePath) {
             this.guid = Guid.Empty;
@@ -26,6 +26,7 @@ namespace SharePointConnect
             this.contactNo = String.Empty;
             this.keyword = String.Empty;
             this.eventNo = String.Empty;
+            this.fileExtension = String.Empty;
 
             foreach(KeyValuePair<string,object> pair in scannedDocument) {
                 if(pair.Value != null) {
@@ -48,9 +49,14 @@ namespace SharePointConnect
                         case "IFUScan2SPEventNum":
                             this.eventNo = pair.Value.ToString();
                             break;
+                        case "File_x0020_Type":
+                            this.fileExtension = pair.Value.ToString();
+                            break;
                     }
                 }
             }
+
+            this.fileName = this.fileName.Replace("." + this.fileExtension.ToLower(), "");
         }
 
         public Guid GetGuid() { return this.guid; }

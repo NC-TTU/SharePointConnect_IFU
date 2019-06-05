63fa07bbbbb8d655e0dbe62134a82fa559a57d49
Timo Tuchczynski <timo.tuchczynski@nasconception.de>
2019-06-05 13:27:12 +0200

    Copyright aktualisiert

--- SharePointConnect/Properties/AssemblyInfo.cs
+++ SharePointConnect/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAS Conception GmbH")]
 [assembly: AssemblyProduct("SharePointConnect")]
-[assembly: AssemblyCopyright("Copyright NAS Conception GmbH ©  2017")]
+[assembly: AssemblyCopyright("Copyright NAS Conception GmbH ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
